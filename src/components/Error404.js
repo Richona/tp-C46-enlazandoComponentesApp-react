be186389d4ed6d3c60bccd005b3bcf4bcb80e11c
Richona <105828822+Richona@users.noreply.github.com>
2022-12-07 14:43:44 -0300

    tp completado

--- src/components/Error404.js
+++ src/components/Error404.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
 export default function Error404() {
-  return (
-    <div>Error404</div>
-  )
+    return (
+        <div className="container-fluid">
+    
+                <h1 className="h3 mb-0 text-gray-800">Error pa</h1>
+                <p>Seleccione otra ruta, esta es incorrecta, hagalo por la salud del gatito.</p>
+                <img src="https://http.cat/404" alt="gatito" />
+
+        </div>
+    )
 }
